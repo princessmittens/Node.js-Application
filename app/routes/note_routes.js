5068f9ee8f57cd3cf6a74fdae6ccda0b47d0dec4
princessmittens <an747455@dal.ca>
2019-01-27 14:10:29 -0400

    Final changes

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -5,19 +5,29 @@ module.exports = function(app, db) {
   
   app.post('/cats', (req, res) => {
     db.collection('cats').save(req.body, (err, result) => {
-      if (err) return console.log(err)
-  
-      console.log('saved to database')
+      if (err) { 
+        return console.log(err)
+      } else { 
+        console.log('saving the cats!')
       res.redirect('/')
+      }
     })
   })
 
 
+  // app.post('/cats', (req, res) => {
+  //   res.send("testing 1, 2, 3")
+  // })
+
+
   app.get('/', (req, res) => {
     db.collection('cats').find().toArray((err, result) => {
-      if (err) return console.log(err)
-      // renders index.ejs
+      if (err) { 
+        return console.log(err) 
+      } else { 
+      console.log('rendering')
       res.render('index.ejs', {cats: result})
+      }
     })
   })
 
--- server.js
+++ server.js
@@ -6,6 +6,12 @@ const app            = express();
 const port = 3000
 app.use(bodyParser.urlencoded({extended: true}))
 
+
+// require('../../../app/routes')(app, {});
+// app.listen(port, () => {
+//   console.log('We are live on ' + port);
+// });
+
 MongoClient.connect(dblk, (err, database) => {
     if (err) return console.log(err)
     db = database.db("lab1")
