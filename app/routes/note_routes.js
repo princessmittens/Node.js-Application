7740c24b18f05a50952edaaab7aed236064bdd01
princessmittens <an747455@dal.ca>
2019-01-26 22:19:05 -0400

    First commit

---
+++ app/routes/note_routes.js
@@ -0,0 +1,27 @@
+
+module.exports = function(app, db) {
+
+
+  
+  app.post('/cats', (req, res) => {
+    db.collection('cats').save(req.body, (err, result) => {
+      if (err) return console.log(err)
+  
+      console.log('saved to database')
+      res.redirect('/')
+    })
+  })
+
+
+  app.get('/', (req, res) => {
+    db.collection('cats').find().toArray((err, result) => {
+      if (err) return console.log(err)
+      // renders index.ejs
+      res.render('lab.ejs', {cats: result})
+    })
+  })
+
+
+  
+
+  
---
+++ server.js
@@ -0,0 +1,17 @@
+const express        = require('express');
+const MongoClient    = require('mongodb').MongoClient;
+const bodyParser     = require('body-parser');
+const dblk            = 'mongodb://mittens:bigdemon1@ds113855.mlab.com:13855/lab1'
+const app            = express();
+const port = 3000
+app.use(bodyParser.urlencoded({extended: true}))
+
+MongoClient.connect(dblk, (err, database) => {
+    if (err) return console.log(err)
+    db = database.db("lab1")
+    require('./app/routes')(app, database);
+    app.listen(port, () => {
+      console.log('We are live on ' + port);
+    });               
+  })
+  
